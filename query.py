20eea6ceffde7ac0b6896db4fd23a3e55c70b943
Partha Kuri <pskuri@gmail.com>
2021-10-24 20:08:32 -0400

    Querying using mongoengine

---
+++ query.py
@@ -0,0 +1,62 @@
+from mongoengine import *
+
+con1 = connect(alias='db1', db='mycustomers')
+con2 = connect(alias='db2', db='db')
+con3 = connect(alias='db3', db='mystudents')
+
+dbs=con1.list_database_names()
+print ('mycustomers dbs:')
+for db in dbs:
+    print (db)
+
+dbs=con2.list_database_names()
+print ('db dbs:')
+for db in dbs:
+    print (db)
+
+dbs=con3.list_database_names()
+print ('mystudents dbs:')
+for db in dbs:
+    print (db)
+
+
+print ("--------------------------------------------------------------------------------")
+
+class teacher(Document):
+    meta = {"db_alias": "db3"}
+    tid=StringField(required=True)
+    name=StringField()
+
+class altstudent(Document):
+    meta = {"db_alias": "db3"}
+    studentid = StringField(required=True)
+    name = StringField()
+    age = IntField(min_value=6, max_value=50)
+    percent = DecimalField(precision=2)
+    email = EmailField()
+    tid=ListField(ReferenceField(teacher))
+    subjects = ListField(StringField())
+    score = DictField()
+
+for altstu in altstudent.objects:
+    print(altstu.studentid)
+    print(altstu.name)
+    print(altstu.age)
+    print(altstu.percent)
+    print(altstu.email)
+    print(altstu.tid)
+    print(altstu.subjects)
+    print(altstu.score)
+
+
+t1=teacher()
+t1.tid='T3'
+t1.name='Hoo'
+# t1.validate()
+
+t2=teacher()
+t2.tid='T4'
+t2.name='Boo'
+
+# teacher.objects.insert([t1,t2])
+# teacher.drop_collection()
